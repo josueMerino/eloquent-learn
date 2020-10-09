36c40290ed5ac8a64a12b018a3a6835b78f3a7a6
josue.merino <josue.merino@alumnos.uneatlantico.es>
2020-10-09 08:48:38 +0200

    First commit

---
+++ database/migrations/2020_10_08_090420_create_taggables_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('tag_id')->unsigned();
+
+            $table->text('body');
+
+            $table->morphs('taggable');
+
+            $table->timestamps();
+
+            $table->foreign('tag_id')->references('id')->on('tags')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
